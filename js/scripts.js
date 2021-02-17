1d66f0fa54c5353fb4b981ff72cff88a39158430
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-02-17 18:10:46 -0300

    Add file via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+//document.querySelector('#earth').setAttribute('src', 'https://github.com/GilbertoASJ/Loopstudios/blob/main/images/mobile/image-deep-earth.jpg?raw=true')
 
+let cardMobile = function() {
+    let firstCardSection = document.querySelector('.first-card-section').style.padding
+    console.log(firstCardSection)
+
+    if(firstCardSection == "") {
+        console.log(firstCardSection)
+
+    } else {
+        document.querySelector('#earth').setAttribute('src', 'https://github.com/GilbertoASJ/Loopstudios/blob/main/images/mobile/image-deep-earth.jpg?raw=true')
+    }
+}
+
+cardMobile()
