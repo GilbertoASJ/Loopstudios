292c57f67b54494693da92f82429cb71df3cba68
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-02-19 17:05:41 -0300

    Update file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 //document.querySelector('#earth').setAttribute('src', 'https://github.com/GilbertoASJ/Loopstudios/blob/main/images/mobile/image-deep-earth.jpg?raw=true')
 
-let cardMobile = function() {
-    let firstCardSection = document.querySelector('.first-card-section').style.padding
-    console.log(firstCardSection)
+let width = window.screen.availWidth
+let height = window.screen.availHeight
 
-    if(firstCardSection == "") {
-        console.log(firstCardSection)
+if(width <= 576) {
+    document.querySelector('.earth').setAttribute('src', './images/mobile/image-deep-earth.jpg')
+    document.querySelector('.arcade').setAttribute('src', './images/mobile/image-night-arcade.jpg')
+    document.querySelector('.soccer').setAttribute('src', './images/mobile/image-soccer-team.jpg')
+    document.querySelector('.grid').setAttribute('src', './images/mobile/image-grid.jpg')
+    document.querySelector('.above').setAttribute('src', './images/mobile/image-from-above.jpg')
+    document.querySelector('.borealis').setAttribute('src', './images/mobile/image-pocket-borealis.jpg')
+    document.querySelector('.curiosity').setAttribute('src', './images/mobile/image-curiosity.jpg')
+    document.querySelector('.fisheye').setAttribute('src', './images/mobile/image-fisheye.jpg')
 
-    } else {
-        document.querySelector('#earth').setAttribute('src', 'https://github.com/GilbertoASJ/Loopstudios/blob/main/images/mobile/image-deep-earth.jpg?raw=true')
-    }
+} else if(width > 576) {
+    document.querySelector('.earth').setAttribute('src', './images/desktop/image-deep-earth.jpg')
+    document.querySelector('.arcade').setAttribute('src', './images/desktop/image-night-arcade.jpg')
+    document.querySelector('.soccer').setAttribute('src', './images/desktop/image-soccer-team.jpg')
+    document.querySelector('.grid').setAttribute('src', './images/desktop/image-grid.jpg')
+    document.querySelector('.above').setAttribute('src', './images/desktop/image-from-above.jpg')
+    document.querySelector('.borealis').setAttribute('src', './images/desktop/image-pocket-borealis.jpg')
+    document.querySelector('.curiosity').setAttribute('src', './images/desktop/image-curiosity.jpg')
+    document.querySelector('.fisheye').setAttribute('src', './images/desktop/image-fisheye.jpg')
 }
 
-cardMobile()
